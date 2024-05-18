aff9c937cdcbcf4d61b5a2a5d4e170e779c404fb
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2024-05-18 15:40:22 +0200

    Added the old Java code

---
+++ src/mhahnFr/NDL/Constants.java
@@ -0,0 +1,35 @@
+/*
+ * NDL4Java - Native Dark mode Listener for Java.
+ *
+ * Copyright (C) 2024  mhahnFr
+ *
+ * This file is part of NDL4Java.
+ *
+ * NDL4Java is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * NDL4Java is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with NDL4Java.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package mhahnFr.NDL;
+
+/**
+ * This class contains the constant values of the library.
+ *
+ * @author mhahnFr
+ * @since 10.05.24
+ */
+public abstract class Constants {
+    private Constants() {}
+
+    /** The name of the native library without extension. */
+    public static final String LIBRARY_NAME = "NDL-native";
+}
